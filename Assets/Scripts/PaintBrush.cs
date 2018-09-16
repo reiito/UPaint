b3e7782654b8225ab269b642bb3506686ddee71b
Rei Ito <Rei.Ito99@gmail.com>
2018-09-17 00:09:43 +1000

    added line drawer

--- Assets/Scripts/PaintBrush.cs
+++ Assets/Scripts/PaintBrush.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PaintBrush : MonoBehaviour
 {
+    public GameObject paintBrushPrefab;
 
+    LineLogic activeLine;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            activeLine = Instantiate(paintBrushPrefab, transform).GetComponent<LineLogic>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            activeLine.UpdateLine(mousePos);
+        }
+    }
 }
