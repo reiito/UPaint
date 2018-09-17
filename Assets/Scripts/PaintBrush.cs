961d9d7d98b124b2c2eb6ede01b590d613ff16d0
Rei Ito <Rei.Ito99@gmail.com>
2018-09-17 16:22:08 +1000

    restructured paint bursh code, added state switching and put in render ordering

--- Assets/Scripts/PaintBrush.cs
+++ Assets/Scripts/PaintBrush.cs
@@ -1,29 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PaintBrush : MonoBehaviour
 {
-    public GameObject paintBrushPrefab;
+    LineRenderer line;
+    EdgeCollider2D edgeCollider;
 
-    LineLogic activeLine;
+    List<Vector2> points;
 
-    private void Update()
+    private void Awake()
     {
-        if (Input.GetMouseButtonDown(0))
+        line = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    public void UpdateLine(Vector2 mousePos)
+    {
+        if (points == null)
         {
-            activeLine = Instantiate(paintBrushPrefab, transform).GetComponent<LineLogic>();
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Vector2.Distance(points.Last(), mousePos) > 0.1f)
         {
-            activeLine = null;
+            SetPoint(mousePos);
         }
+    }
 
-        if (activeLine != null)
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+        line.positionCount = points.Count;
+        line.SetPosition(points.Count - 1, point);
+
+        if (points.Count > 1)
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            activeLine.UpdateLine(mousePos);
+            edgeCollider.points = points.ToArray();
         }
     }
 }
--- Assets/Scripts/PaintManager.cs
+++ Assets/Scripts/PaintManager.cs
@@ -1,35 +1,90 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+public enum Tools
+{
+    BRUSH, ERASER, CIRCLE, SQUARE
+}
+
 public class PaintManager : MonoBehaviour
 {
-  public Transform defaultColor;
+    public Transform defaultColor;
 
-  public GameObject circleBrush;
+    public GameObject paintBrush;
+    public GameObject circleBrush;
 
-  Transform activeColorButton;
-  Material brushMat;
+    PaintBrush activeLine;
 
-  private void Start()
-  {
-    activeColorButton = defaultColor;
-    SetCurrentColor(defaultColor);
-  }
+    Transform activeColorButton;
+    Material brushMat;
 
-  private void Update()
-  {
-    if (Input.GetMouseButton(0))
+    Tools selectedTool;
+
+    int layerCount = 0;
+
+    private void Start()
     {
-      SpriteRenderer sprite = Instantiate(circleBrush, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f)), Quaternion.identity, transform).GetComponent<SpriteRenderer>();
-      sprite.material = brushMat;
-    } 
-  }
+        activeColorButton = defaultColor;
+        SetCurrentColor(defaultColor);
+        selectedTool = Tools.BRUSH;
+    }
 
-  public void SetCurrentColor(Transform colorButton)
-  {
-    activeColorButton.GetChild(0).gameObject.SetActive(false);
-    brushMat = colorButton.gameObject.GetComponent<Image>().material;
-    activeColorButton = colorButton.transform;
-    colorButton.GetChild(0).gameObject.SetActive(true);
-  }
+    private void Update()
+    {
+        switch (selectedTool)
+        {
+            case Tools.BRUSH:
+                PaintBrush();
+                break;
+            case Tools.ERASER:
+                break;
+            case Tools.CIRCLE:
+                PaintCircle();
+                break;
+            case Tools.SQUARE:
+                break;
+        }
+    }
+
+    public void SetCurrentColor(Transform colorButton)
+    {
+        activeColorButton.GetChild(0).gameObject.SetActive(false);
+        brushMat = colorButton.gameObject.GetComponent<Image>().material;
+        activeColorButton = colorButton.transform;
+        colorButton.GetChild(0).gameObject.SetActive(true);
+    }
+
+    void PaintBrush()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            layerCount++;
+            activeLine = Instantiate(paintBrush, transform).GetComponent<PaintBrush>();
+            activeLine.GetComponent<LineRenderer>().material = brushMat;
+            activeLine.GetComponent<LineRenderer>().material.renderQueue += layerCount;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            activeLine.UpdateLine(mousePos);
+        }
+    }
+
+    void PaintCircle()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            layerCount++;
+            SpriteRenderer sprite = Instantiate(circleBrush, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f)), Quaternion.identity, transform).GetComponent<SpriteRenderer>();
+            sprite.material = brushMat;
+            sprite.material.renderQueue += layerCount;
+        }
+    }
 }
