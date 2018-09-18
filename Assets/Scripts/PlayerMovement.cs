f4307ccdad7684286bbc7bbf2d9784e7675a68af
Rei Ito <Rei.Ito99@gmail.com>
2018-09-19 04:10:24 +1000

    hoping deleted gitignore will fix the prefab issue, animations in now to do camera, switching and pickups for square tool

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,39 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController2D controller;
+  public CharacterController2D controller;
+  public Animator animator;
 
-    public float runSpeed = 40f;
+  public float runSpeed = 40f;
 
-    float horizontalMove = 0f;
-    bool jump = false;
-    bool crouch = false;
+  float horizontalMove = 0f;
+  bool jump = false;
+  bool crouch = false;
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+
+    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+    if (Input.GetButtonDown("Jump"))
     {
-
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
-
-        //if (Input.GetButtonDown("Crouch"))
-        //{
-        //	crouch = true;
-        //} else if (Input.GetButtonUp("Crouch"))
-        //{
-        //	crouch = false;
-        //}
-
+      jump = true;
+      animator.SetBool("IsJumping", true);
     }
 
-    void FixedUpdate()
-    {
-        // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
-    }
+    //if (Input.GetButtonDown("Crouch"))
+    //{
+    //	crouch = true;
+    //} else if (Input.GetButtonUp("Crouch"))
+    //{
+    //	crouch = false;
+    //}
+
+  }
+
+  public void OnLanding()
+  {
+    animator.SetBool("IsJumping", false);
+  }
+
+  void FixedUpdate()
+  {
+    // Move our character
+    controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    jump = false;
+  }
 }
